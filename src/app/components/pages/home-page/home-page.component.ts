36beef047bcac99bf3e32d97bb1bd45ae30f4ef2
baonvt1910 <baonvt1910@gmail.com>
2023-04-13 11:03:47 +0700

    updatecontsole

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router  } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { FoodService } from "src/app/services/food.service";
 import { Food } from "src/app/shared/models/food";
 import { CartService } from "src/app/services/cart.service";
@@ -13,7 +13,7 @@ export class HomePageComponent implements OnInit {
   foods: Food[] = [];
   constructor(
     private foodService: FoodService,
-    private cartService : CartService,
+    private cartService: CartService,
     activatedRoute: ActivatedRoute,
     private router: Router
   ) {
@@ -29,9 +29,9 @@ export class HomePageComponent implements OnInit {
   }
   ngOnInit(): void {}
 
-  addToCart(food){
-    console.log(food)
-     this.cartService.addToCart(food);
-     this.router.navigateByUrl('/cart-page');
+  addToCart(food) {
+    // console.log(food);
+    this.cartService.addToCart(food);
+    this.router.navigateByUrl("/cart-page");
   }
 }
