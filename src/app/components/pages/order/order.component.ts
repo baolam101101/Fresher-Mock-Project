f65a4fcce31d013cd091fed3eff96b51ab0602dd
Quan123781 <130044896+Quan2203@users.noreply.github.com>
2023-04-25 09:50:14 +0700

    code mới

--- src/app/components/pages/order/order.component.ts
+++ src/app/components/pages/order/order.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { CartService } from "src/app/services/cart.service";
+import { Cart } from "src/app/shared/models/Cart";
+import { CartItem } from "src/app/shared/models/CartItem";
+import { Food } from "src/app/shared/models/Food";
+import { sample_foods } from "src/data";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  cart!: Cart;
+  // options: Dropdown[];
 
+  constructor(
+    private cartService: CartService,
+    activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+    const form = document.querySelector("form");
+    const quantityInput = document.querySelector('#quantity') as HTMLInputElement;
+    
+    
+  }
+
+
+  ngOnInit(): void {}
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
+
+  selectOption(value: any) {
+    console.log(value);
+    if (value === 0) {
+      this.router.navigate(["/logout"]);
+    } else if (value === 1) {
+      this.router.navigate(["/userInformation"]);
+    } else {
+      this.router.navigate(["/order"]);
+    }
+  }
+
+  func() {
+    console.log("s");
+  }
+
+  foods = sample_foods;
+  selectedFood?: Food;
+
+  onSelect(item: any): void {
+    this.selectedFood = item.food;
+  }
 }
